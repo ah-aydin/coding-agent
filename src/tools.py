5b30cd786548477e142e208bcc2803a4f6d779c8
Ahmed-Hamza Aydin <ahamzaaydin.py@gmail.com>
2025-09-29 17:11:38 +0300

    List files tool

--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Callable
+import os
 
 TOOL_FUNCS: dict[str, Callable[[dict], dict]] = {}
 TOOLS = []
@@ -43,12 +44,44 @@ def read_file(args):
         file_content = '\n'.join(lines)
     return { 'file_content': file_content }
 
+def list_directory(args):
+    path = args.get('path', '.')
+    files = []
+    folders = []
+    for item_path in os.listdir(path):
+        full_path = os.path.join(path, item_path)
+        if os.path.isfile(full_path):
+            files.append(full_path)
+        else:
+            folders.append(full_path)
+    return {
+        'files': files,
+        'folders': folders,
+    }
+
 def init():
     define_function_tool(
         'read_file',
         "Read the contents of a given relative file path. Use this when you want to see what's inside a file. Do not use this with directory names.",
         read_file,
         [
-            ToolParameter(name='file_path', type='string', description='Relative file path of the file to read')
+            ToolParameter(
+                name='file_path',
+                type='string',
+                description='Relative file path of the file to read'
+            )
+        ]
+    )
+    define_function_tool(
+        'list_directory',
+        "List files and directories at a given path. If no path is provided, lists files in the current directory.",
+        list_directory,
+        [
+            ToolParameter(
+                name='path',
+                type='string',
+                description='Optional relative path to list files from. Defaults to current directory if not provided.',
+                required=False
+            )
         ]
     )
